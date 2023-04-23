59b437047b749d397ce4288188d69ea0dea88e5c
rongliyuan <rongliyuan@gmail.com>
2023-04-23 23:15:19 +0200

    Update preprocessing.py
    Address the issues discussed in https://github.com/dnwissel/sib_ai_benchmark/pull/2.

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -3,32 +3,98 @@ from pathlib import Path
 import logging
 import scanpy as sc
 import anndata as ad
-import numpy as np
-import warnings
-
-warnings.filterwarnings("ignore")
 
 logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s: %(message)s"
+    level=logging.INFO, format="%(asctime)s - %(levelname)s: %(message)s"
 )
 sc.settings.verbosity = 2
 sc.logging.print_header()
 
 
+def split_data_by_category(data, category):
+    category_counts = data.obs[category].value_counts()
+    category_data_dict = {
+        c: data[data.obs[category] == c] for c in category_counts.index
+    }
+    return category_counts, category_data_dict
+
+
+def preprocess_per_batch(batch, args):
+    sc.pp.filter_cells(batch, min_genes=args.min_genes)
+    sc.pp.filter_genes(batch, min_cells=args.min_cells)
+    if args.mt_start is not None and args.gene_name_column is not None:
+        batch.var["mt"] = batch.var[args.gene_name_column].str.startswith(args.mt_start)
+    else:
+        batch.var["mt"] = False
+    sc.pp.calculate_qc_metrics(
+        batch, qc_vars=["mt"], percent_top=None, log1p=False, inplace=True
+    )
+    batch = batch[
+        (batch.obs.n_genes_by_counts < args.n_genes_by_counts_upper)
+        & (batch.obs.pct_counts_mt < args.pct_counts_mt_upper)
+    ]
+    batch.layers["preprocessed"] = batch.X.copy()
+    sc.pp.normalize_total(
+        batch, target_sum=args.normalize_target_sum, layer="preprocessed"
+    )
+    sc.pp.log1p(batch, layer="preprocessed")
+    if args.filter_hvg:
+        sc.pp.highly_variable_genes(
+            batch,
+            layer="preprocessed",
+            min_mean=args.filter_hvg_params[0],
+            max_mean=args.filter_hvg_params[1],
+            min_disp=args.filter_hvg_params[2],
+        )
+    return batch
+
+
 def main():
     p = Path(__file__).parents[1]
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description="Set the parameters for preprocessing.",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
     parser.add_argument(
         "--read_path",
-        type=lambda i: p.joinpath(i),
-        default=p.joinpath("data-raw/ASAP41_final.h5ad"),
-        help="Path to read the raw data",
+        type=lambda i: p.joinpath("data-raw", i),
+        # default=p.joinpath("data-raw", "ASAP41_final.h5ad"),  # ASAP
+        default=p.joinpath("data-raw", "SRP200614.h5ad"),  # Bgee
+        help="Path to read the data file",
     )
     parser.add_argument(
         "--save_path",
-        type=lambda i: p.joinpath(i),
-        default=p.joinpath("data"),
+        type=lambda i: p.joinpath("data", i),
+        # default=p.joinpath("data", "asap"),  # ASAP
+        default=p.joinpath("data", "bgee"),  # Bgee
         help="Path to save the preprocessed data",
+    )
+    parser.add_argument(
+        "--tissue_column",
+        type=str,
+        # default="asap_tissue",  # ASAP
+        default=None,  # Bgee
+        help="Column name of tissues. \nIf not provided, will be set to None.",
+    )
+    parser.add_argument(
+        "--tissue_name",
+        type=str,
+        # default="unknown_tissue",  # ASAP
+        default="gut",  # Bgee
+        help="If there is no tissue column \nand all the data is from the same known tissue,\nplease specify the tissue name. \nIf not provided, will be set to 'unknown_tissue'.",
+    )
+    parser.add_argument(
+        "--batch_column",
+        type=str,
+        # default="batch",  # ASAP
+        default=None,  # Bgee
+        help="Column name of batches. \nIf not provided, will be set to None.",
+    )
+    parser.add_argument(
+        "--batch_name",
+        type=str,
+        default="unknown",
+        help="If there is no batch column \nand all the data is from the same known batch,\nplease specify the batch name. \nIf not provided, will be set to 'unknown_batch'.",
     )
     parser.add_argument(
         "--min_genes",
@@ -43,23 +109,30 @@ def main():
         help="Keep genes that are found in >= (min_cells) cells",
     )
     parser.add_argument(
-        "--mt_start",
+        "--gene_name_column",
         type=str,
-        default="mt:",
-        help="Mitochondrial genes names start with (mt_start)",
+        # default="gene_name",  # ASAP
+        default=None,  # Bgee
+        help="Column of gene names in var. \nIf not provided, will be set to None.",
     )
     parser.add_argument(
-        "--n_genes_qt",
-        type=float,
-        nargs=2,
-        default=[0.02, 0.98],
-        help="Set the lower and upper quantile limit of n_genes_by_counts",
+        "--mt_start",
+        type=str,
+        # default="mt:",  # ASAP
+        default=None,  # Bgee
+        help="Mitochondrial genes names start with (mt_start). \nIf not provided, will be set to None.",
+    )
+    parser.add_argument(
+        "--n_genes_by_counts_upper",
+        type=int,
+        default=2500,
+        help="Set the upper limit of the number of genes expressed in the counts matrix (n_genes_by_counts)",
     )
     parser.add_argument(
         "--pct_counts_mt_upper",
         type=float,
         default=20,
-        help="Set the upper limit of pct_counts_mt",
+        help="Set the upper limit of the percentage of counts in mitochondrial genes",
     )
     parser.add_argument(
         "--normalize_target_sum",
@@ -68,69 +141,53 @@ def main():
         help="Normalize the sum of counts in every cell to (normalize_target_sum) UMI",
     )
     parser.add_argument(
-        "--tissue_col", type=str, default="asap_tissue", help="Column name of tissues"
+        "--filter_hvg",
+        type=lambda i: True if i.lower() == "y" else False,
+        default=False,
+        help="Filter highly variable genes [y/n]",
     )
     parser.add_argument(
-        "--batch_col", type=str, default="batch", help="Column name of batches"
+        "--filter_hvg_params",
+        type=float,
+        nargs=3,
+        default=[0.0125, 3, 0.5],
+        help="Set the parameters min_mean, max_mean, min_disp for identifying highly variable genes. \nDefault: 0.0125, 3, 0.5",
     )
     args = parser.parse_args()
 
     data = ad.read_h5ad(args.read_path)
     logging.info(f"data.shape: {data.shape}")
-
-    def split_data_by_category(data, category):
-        category_counts = data.obs[category].value_counts()
-        category_data_dict = {
-            c: data[data.obs[category] == c] for c in category_counts.index
-        }
-        return category_counts, category_data_dict
-
-    def preprocess_per_batch(batch):
-        sc.pp.filter_cells(batch, min_genes=args.min_genes)
-        # sc.pp.filter_genes(batch, min_cells=args.min_cells)
-        batch.var["mt"] = batch.var.gene_name.str.startswith(args.mt_start)
-        sc.pp.calculate_qc_metrics(
-            batch, qc_vars=["mt"], percent_top=None, log1p=False, inplace=True
+    if args.tissue_column is None:
+        tissue_dict = {args.tissue_name: data}
+        logging.info(f"tissue_counts: {args.tissue_name}  {data.shape[0]}")
+    else:
+        tissue_counts, tissue_dict = split_data_by_category(
+            data, category=args.tissue_column
         )
-        n_genes = batch.obs.n_genes_by_counts
-        lower_lim = np.quantile(n_genes, args.n_genes_qt[0])
-        upper_lim = np.quantile(n_genes, args.n_genes_qt[1])
-        batch = batch[
-            (batch.obs.n_genes_by_counts.between(lower_lim, upper_lim))
-            & (batch.obs.pct_counts_mt < args.pct_counts_mt_upper)
-        ]
-        sc.pp.normalize_total(batch, target_sum=args.normalize_target_sum)
-        sc.pp.log1p(batch)
-        sc.pp.highly_variable_genes(batch, min_mean=0.0125, max_mean=3, min_disp=0.5)
-        batch.raw = batch
-        return batch
+        logging.info(f"tissue_counts: {tissue_counts}")
 
-    tissue_counts, tissue_dict = split_data_by_category(data, category=args.tissue_col)
-    logging.info(f"tissue_counts: {tissue_counts}")
-    tissue_pp_dict = {}
     for tissue_, tissue_data in tissue_dict.items():
-        sc.pp.filter_genes(tissue_data, min_cells=args.min_cells)
-        logging.info(
-            f"tissue {tissue_} sc.pp.filter_genes -> tissue_data.shape = {tissue_data.shape}"
-        )
-        batch_counts, batch_data_dict = split_data_by_category(
-            tissue_data, category=args.batch_col
-        )
-        logging.info(f"tissue {tissue_} batch_counts: {batch_counts}")
+        if args.batch_column is None:
+            batch_dict = {args.batch_name: tissue_data}
+            logging.info(
+                f"tissue {tissue_} batch_counts: {args.batch_name}  {tissue_data.shape[0]}"
+            )
+        else:
+            batch_counts, batch_dict = split_data_by_category(
+                tissue_data, category=args.batch_column
+            )
+            logging.info(f"tissue {tissue_} batch_counts: {batch_counts}")
         Path(args.save_path.joinpath(f"{tissue_}")).mkdir(parents=True, exist_ok=True)
         batch_pp_dict = {}
-        for batch_, batch_data in batch_data_dict.items():
-            batch_pp = preprocess_per_batch(batch_data)
+        for batch_, batch_data in batch_dict.items():
+            batch_pp = preprocess_per_batch(batch_data, args)
             logging.info(f"batch {batch_} batch_pp.shape = {batch_pp.shape}")
             batch_pp_dict[batch_] = batch_pp
             save_path = args.save_path.joinpath(
-                f"{tissue_}", f"asap_{tissue_}_batch{batch_}_preprocessed.h5ad"
+                f"{tissue_}", f"{tissue_}_batch_{batch_}_pp.h5ad"
             )
             batch_pp.write(save_path)
-            logging.info(f"batch{batch_}_preprocessed.h5ad saved to {save_path}")
-        tissue_pp_dict[tissue_] = batch_pp_dict
-
-    return tissue_pp_dict
+            logging.info(f"batch_{batch_}_pp.h5ad saved to {save_path}")
 
 
 if __name__ == "__main__":
