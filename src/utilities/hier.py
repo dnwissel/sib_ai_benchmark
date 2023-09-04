c632e2ce5639aa6ead61bce4140388743e98ed1b
Ming Yi <myi@eu-login-31.euler.ethz.ch>
2023-09-04 09:54:34 +0200

    change get_R

--- src/utilities/hier.py
+++ src/utilities/hier.py
@@ -82,7 +82,7 @@ class Encoder:
         # self.label_idx = np.array(list(map( self.node_map.get, nodes.unique())))
         return Y
 
-    def get_R(self):
+    def get_R(self, tensor=True):
         # Compute matrix of ancestors R
         # Given n classes, R is an (n x n) matrix where R_ij = 1 if class i is descendant of class j
         nodes = self.G_idx.nodes()
@@ -94,11 +94,13 @@ class Encoder:
             ancestors = list(nx.ancestors(self.G_idx, i))
             if ancestors:
                 R[i, ancestors] = 1
-
-        R = torch.tensor(R)
-        #Transpose to get the descendants for each node
-        R = R.transpose(1, 0)
-        R = R.unsqueeze(0)
+                
+        if tensor:
+            R = torch.tensor(R)
+            #Transpose to get the descendants for each node
+            R = R.transpose(1, 0)
+            R = R.unsqueeze(0)
+            return R
         return R
 
 
