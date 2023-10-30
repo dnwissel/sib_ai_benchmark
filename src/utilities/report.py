44c5a7abf34ebe4bb08aaa655a40fe38e07887bb
Ming Yi <myi@eu-login-48.euler.ethz.ch>
2023-10-30 08:51:55 +0100

    code for report

--- src/utilities/report.py
+++ src/utilities/report.py
@@ -8,8 +8,16 @@ import pickle
 
 def highlight_max(row):
     max_value = max(row)
-    bold_max_value = f"\\textbf{{{max_value}}}"
-    return [bold_max_value if x == max_value else x for x in row]
+    max_value_format = "{:.4f}".format(max_value)
+    bold_max_value = f"\\textbf{{{max_value_format}}}"
+
+    for idx, val in enumerate(row):
+        if val == max_value:
+            row[idx] = bold_max_value
+        else:
+            row[idx] = "{:.4f}".format(val)
+
+    return row
 
 # formatted_matrix = np.apply_along_axis(highlight_max, axis=1, arr=matrix)
 
@@ -20,9 +28,16 @@ def get_table(results, metric_name):
     info = {}
 
     tissue_names = list(results['datasets'].keys())
+
+    try:
+        tissue_names.remove('gut')
+    except:
+        pass
+
     tissue_names = sorted(tissue_names)
 
     model_names = results['datasets'][tissue_names[0]]['model_results'].keys()
+    model_names = [mn for mn in model_names if mn != 'NaiveBayes']
     model_names = sorted(model_names)
 
     info['labels'] = list(model_names)
@@ -37,7 +52,8 @@ def get_table(results, metric_name):
             if metric_name in ['ece', 'ece_uc']:
                 res_model.append(res_tissue[mn][metric_name])
             else:
-                median = round(res_tissue[mn]['scores'][metric_name]['median'], 4)
+                median = res_tissue[mn]['scores'][metric_name]['median']
+                median = round(median, 4)
                 res_model.append(median)
         data.append(res_model)
 
@@ -45,7 +61,11 @@ def get_table(results, metric_name):
     data = np.array(data, dtype='object')
     # TODO add bold
     data = np.apply_along_axis(highlight_max, axis=1, arr=data)
-    print(data.shape)
+    # print(data.shape)
+
+    idx = tissue_names.index('proboscis_and_maxpalp')
+    tissue_names[idx] = 'proboscis\_and\_maxpalp'
+
     tissue_names = np.array(tissue_names)
     table = np.insert(data, 0, tissue_names, axis=1)
     return table, model_names
@@ -58,7 +78,8 @@ def table_code(table, headers):
     Code for overleaf
     """
     # table = [["Alice", 24], ["Bob", 19], ["Charlie", 32]]
-    print(tabulate(table, headers=headers, numalign="center", tablefmt="latex"))
+    code = tabulate(table, headers=headers, numalign="center", tablefmt="latex_raw")
+    print(code)
 
 
 # TODO: ACC against ece
@@ -81,10 +102,13 @@ if __name__ == "__main__":
     print(fns)
 
     # ece
+    print("\\begin{table}[h]")
+    print("\t\\centering")
+
     for fn in fns:
         # if 'path-eval' in fn  or 'global' in fn:
             # continue
-
+        print('\n')
         with open(path_res + f'/{fn}', 'rb') as fh:
             results = pickle.load(fh)
         metric_name = 'F1_SCORE_MACRO'.lower()
@@ -93,4 +117,10 @@ if __name__ == "__main__":
         header = ['Tissue']
         header.extend(model_names)
         table_code(table, header)
-        break
+        fn = fn[:-4]
+        fn = fn.replace('_', '\_')
+        print(f"\\caption{{{fn}}}")
+        # print('\n')
+
+    print("\\end{table}")
+        # break
